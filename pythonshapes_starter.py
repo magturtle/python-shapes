990d68feb7f22f19487b31f153e28194ab9eed10
magturtle <mag.laton@gmail.com>
2017-07-06 14:40:02 -0500

    Add files via upload

--- pythonshapes_starter.py
+++ pythonshapes_starter.py
@@ -12,45 +12,48 @@ t.setposition(x_pos, y_pos)
 
 ### Write your code below
 
-delay (50)
 
 
-showturtle()
-begin_fill()
-pendown()
-pensize (5)
-pencolor("red")
-forward(100)
-left(90)
-forward(100)
-left(90)
-forward(100)
-left(90)
-forward(100)
-left(90)
+for i in range (5):
+    delay (50)
 
-color("violet")
-end_fill()
-penup()
 
-time.sleep(2)
-
-reset()
-
-time.sleep(1)
-begin_fill()
-pendown()
-pensize (5)
-pencolor("green")
-for i in range(3):
+    showturtle()
+    begin_fill()
+    pendown()
+    pensize (5)
+    pencolor("red")
     forward(100)
-    left(120)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
 
-color("yellow")
-end_fill()
+    color("violet")
+    end_fill()
+    penup()
+
+    time.sleep(2)
+
+    reset()
+
+    time.sleep(1)
+    begin_fill()
+    pendown()
+    pensize (5)
+    pencolor("green")
+    for i in range(3):
+        forward(100)
+        left(120)
+
+    color("yellow")
+    end_fill()
 
 
 
 
-# Close window on click.
-exitonclick()
+    # Close window on click.
+    exitonclick()
